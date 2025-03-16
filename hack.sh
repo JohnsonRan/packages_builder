e710c23baa020be0ef88e6cd36f099aa0b2cc607
JohnsonRan <Johnson_Ran@hotmail.com>
2025-03-16 09:55:47 +0800

    hack more Signed-off-by: JohnsonRan <me@ihtw.moe>

--- hack.sh
+++ hack.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
 
+#duck deps
 sed -i 's/$(BPF_DEPENDS)//g' sources/InfinityDuck/duck/Makefile
+# v2raygeodata date
+sed -i "s/GEOX_VER:=.*/GEOX_VER:=$(date +%Y%m%d)/" sources/packages_net_v2ray-geodata/Makefile
